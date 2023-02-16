2fced69b712bc34fc3b489866d3476f1cd917f07
AKHIL-S123 <amwonders1@gmail.com>
2023-02-16 20:54:13 +0530

    f2

---
+++ mk/ss/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from . models import  Cart, Product, Customer, OrderPlaced, Payment, Product
+
+# Register your models here.
+
+@admin.register(Product)
+class ProductModeAdmin(admin.ModelAdmin):
+    list_display = [ 'id','title','discounted_price','category','product_image']
+
+@admin.register(Customer)
+class CustomerModelAdmin(admin.ModelAdmin):
+    list_display = ['id','user','locality','city','mobile','state','zipcode'] 
+
+@admin.register(Cart)
+class CartModelAdmin(admin.ModelAdmin):
+    list_display = ['id','user','product','quantity']
+
+@admin.register(Payment)
+class PaymentModeAdmin(admin.ModelAdmin):
+    list_display = ['id','user','amount','razorpay_order_id','razorpay_payment_status','razorpay_payment_id','paid']
+
+@admin.register(OrderPlaced)
+class OrderPlacedModelAdmin(admin.ModelAdmin):
+    list_display=['id','user','customer','product','quantity','ordered_date','status','payment']
